12824b5ffefaf57a2960cd691c31342d4a098b91
DiegoLopez25 <diegoenriquecortezlopez@gmail.com>
2022-02-28 18:00:23 -0600

    agregando el proyecto

---
+++ app/Views/widget/index.php
@@ -0,0 +1,25 @@
+<?= $this->extend('templates/admin_template')?>
+<?= $this->section('content')?>
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Widgets</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Widgets</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <h1>Estas en Widget</h1>
+</section>
+<!-- /.content -->
+<?= $this->endSection()?>
